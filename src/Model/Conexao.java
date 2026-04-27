36ff033174d0320523ee05cffaead0c2d83b56a5
Leonardo <158120970+leonardonapoless@users.noreply.github.com>
2026-04-27 10:53:43 -0300

    add connection

---
+++ src/Model/Conexao.java
@@ -0,0 +1,8 @@
+package model;
+
+public class Conexao {
+	private static String server = "jdbc:sqlserver://10.109.8.9:1499;";
+	private static String banco = "databaseName=POO_gp01";
+	private static String usuario = "user=POO_gp01;password=;encrypt=false;trustServerCertificate\n"
+			+ "=true;loginTimeout=30;";
+}
